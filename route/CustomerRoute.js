48f888b374682682b8180ab2274669621edd8444
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-08-25 10:38:48 +0530

    create save method

--- route/CustomerRoute.js
+++ route/CustomerRoute.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const CustomerController = require('../controller/Customer');
 
 router.post('/save', CustomerController.saveCustomer);
-router.put('/update', CustomerController.updateCustomer);
-router.get('/get/:userId', CustomerController.getCustomer);
-router.delete('/delete/:userId', CustomerController.deleteCustomer);
+// router.put('/update', CustomerController.updateCustomer);
+// router.get('/get/:userId', CustomerController.getCustomer);
+// router.delete('/delete/:userId', CustomerController.deleteCustomer);
 
 
 module.exports = router;
